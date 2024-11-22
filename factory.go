7c771648445d1cb4395352f41aec7158d6aa0af3
Andy Evans <andy.evans@cloudticity.com>
2024-11-21 18:57:12 -0600

    Initial

---
+++ factory.go
@@ -0,0 +1,43 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package awscloudwatchmetricsreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/awscloudwatchmetricsreceiver"
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver"
+
+	"gitlab.cloudticity.com/platform/otel/receivers/aws-cloudwatch-metrics.git/internal/metadata"
+)
+
+// NewFactory creates a factory for awscloudwatchmetrics receiver.
+func NewFactory() receiver.Factory {
+	return receiver.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		receiver.WithMetrics(createMetricsReceiver, metadata.MetricsStability),
+	)
+}
+
+func createMetricsReceiver(
+	_ context.Context,
+	params receiver.Settings,
+	baseCfg component.Config,
+	consumer consumer.Metrics,
+) (receiver.Metrics, error) {
+	cfg := baseCfg.(*Config)
+	rcvr := newMetricReceiver(cfg, params, consumer)
+	return rcvr, nil
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		PollInterval: defaultPollInterval,
+		Metrics: &MetricsConfig{
+			NamedMetrics: []NamedMetricConfig{},
+		},
+	}
+}
